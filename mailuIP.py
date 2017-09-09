c53b37ccf843fd93919499dd79e8d99d0d64ecd1
jay <schenxmu@gmail.com>
2017-09-09 20:17:24 +0800

    more robust

--- mailuIP.py
+++ mailuIP.py
@@ -17,11 +17,13 @@ def _format_addr(s):
         addr.encode('utf-8') if isinstance(addr, unicode) else addr))
 
 def getConf():
-    with open('conf.json', 'r') as f:
+    # crontab needs absolute path
+    abspath = '/home/jay/projects/mailuIP/conf.json'
+    with open(abspath, 'r') as f:
         conf = json.load(f)
     return conf
 
-def sendMail(ip):
+def sendMail(ip_list):
     conf = getConf()
     # print 'now ip:', ip
     # print 'conf:', conf
@@ -29,11 +31,17 @@ def sendMail(ip):
     password = conf['passwd']
     to_addr = conf['receivers']
     smtp_server = conf['SMTPServer']
+    
+    IPmsg = ''
+    for ind, ip in enumerate(ip_list):
+        IPmsg += 'IP ' + str(ind) + ' : ' + ip# + '\n'
+    msg = MIMEText('hello, 创新大厦A区305外网IP已变化。\n' + IPmsg +
+            'Sent by 创新大厦的 Python...', 'plain', 'utf-8')
+    # print IPmsg
+    msg['From'] = _format_addr(u'IP管理员 <%s>' % from_addr)
+    msg['To'] = _format_addr(u'创新大厦用户 <%s>' % to_addr)
 
-    msg = MIMEText('hello, 创新大厦A区305外网IP已变化。\nsend by 创新大厦的 Python...', 'plain', 'utf-8')
-    msg['From'] = _format_addr(u' <%s>' % from_addr)
-    msg['To'] = _format_addr(u'IP管理员 <%s>' % to_addr)
-    title = u'创新大厦IP : ' + ip
+    title = u'创新大厦 IP'
     msg['Subject'] = Header(title, 'utf-8').encode()
 
     '''
@@ -50,27 +58,43 @@ def sendMail(ip):
     # server.connect(smtp_server, 587)
     server.ehlo()
     server.login(from_addr, password)
-    server.sendmail(from_addr, [to_addr], msg.as_string())
+    server.sendmail(from_addr, to_addr.split(','), msg.as_string())
     server.quit()
 
 def getIP():
-    tmp = os.popen('curl ifconfig.me').readlines()
-    ip = tmp[0].strip('\n')
+    tmp1 = os.popen('wget http://ipecho.net/plain -O - -q ; echo').readlines()
+    # print tmp1
+    if tmp1 == []:
+        tmp2 = os.popen('curl http://members.3322.org/dyndns/getip').readlines()
+        ip = tmp2[0]#.strip('\n')
+    else:
+        ip = tmp1[0]#.strip('\n')
     return ip
 
 def compareIP(ip):
-    with open('ip.txt', 'r') as f:
-        old_ip = f.read().strip('\n')
-        # print 'old_ip:', old_ip
-    if old_ip != ip:
-        with open('ip.txt', 'w') as f:
-            f.write(ip)
-        return True
+    # crontab needs absolute path
+    abspath = '/home/jay/projects/mailuIP/ip.txt'
+    with open(abspath, 'r') as f:
+        old_ip = f.readlines()
+        #print 'old_ip:', old_ip
+
+    if ip not in old_ip:
+        # only has one ip
+        if len(old_ip) == 1:
+            old_ip.append(ip)
+            # print old_ip
+            with open(abspath, 'w') as f:
+                f.writelines(old_ip)
+            return old_ip
+        else:
+            with open(abspath, 'w') as f:
+                f.write(ip)
+            return [ip]
     else:
         return False
 
 if __name__ == '__main__':
-    ip = getIP()
-    # print ip
-    if compareIP(ip):
-        sendMail(ip)
+    now_ip = getIP()
+    ip_list = compareIP(now_ip)
+    if ip_list:
+        sendMail(ip_list)
